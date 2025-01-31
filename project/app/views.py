e01cd6aa6c79ea14128e5dcc32b7e44f678a7980
San21 <san095238@gmail.com>
2025-01-31 09:40:37 +0530

    product items and category are added

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from items.models import Category , Item
+
+
 def index(request):
-    return render(request, 'app/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categoryies = Category.objects.all()
+
+    return render(request, 'app/index.html' ,{
+        'categories' : categoryies,
+        'items' : items,
+    })
 
 def contact(request):
     return render(request, 'app/contact.html')
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from app.views import index ,contact
 
 
@@ -9,4 +12,4 @@ urlpatterns = [
     path('',index, name='index'),
      path('contact/',contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
