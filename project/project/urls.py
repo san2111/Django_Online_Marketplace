b94c019325996c286b7b5f8516e63b5cd87132ed
San21 <san095238@gmail.com>
2025-02-02 10:08:10 +0530

    add detail page..

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path ,include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,6 +10,7 @@ from app.views import index ,contact
 
 urlpatterns = [
     path('',index, name='index'),
-     path('contact/',contact, name='contact'),
+    path('contact/',contact, name='contact'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('items/',include('items.urls')),
+ ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
